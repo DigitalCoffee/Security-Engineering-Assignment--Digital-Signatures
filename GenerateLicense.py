9647310e0192db354e1187eb3d435cd15aa4521d
DigitalCoffee <a.hansen00@gmail.com>
2015-11-09 11:58:20 -0800

    License Generation & Validation
    Added the two python scripts:
    -GenerateLicense.py
    -Validate.py

---
+++ GenerateLicense.py
@@ -0,0 +1,56 @@
+﻿from sys import exit, argv
+import getopt
+from datetime import date, timedelta
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+from Crypto.Random import random
+
+usage = "GenerateLicense.py {-c|-t}"
+corruption = ""
+addDays = 30
+
+try:
+	opts, args = getopt.getopt(argv[1:],"ct")
+except getopt.GetoptError:
+	print usage
+	exit(1)
+for opt, arg in opts:
+	if opt in ("-c"):
+		corruption = str(random.getrandbits(19))
+	elif opt in ("-t"):
+		addDays = random.randint(-100, -1)
+
+
+# ---- Begin message generation ----
+
+
+# Make the license expire in 30 days
+expDate     = date.today() + timedelta(days=addDays)
+
+# Include public key to decrypt signature
+publicKey   = open('pubkey.der').read()
+
+# Message body
+licenseStr  = "EXP_DATE:\n" + str(expDate) + "\nKEY:\n" + publicKey + "\n"
+
+# Write the expiry date + "vendor's" public key
+license     = open("license", 'w+')
+license.write(licenseStr)
+
+
+# ---- Begin digital signature generation ----
+
+
+# Import private key
+key         = RSA.importKey(open('privkey.der').read())
+
+# Hash the message
+hash        = SHA.new(str(expDate) + corruption)
+
+# Create signer and sign the hash
+signer      = PKCS1_v1_5.new(key)
+signature   = signer.sign(hash)
+
+# Write the digital signature
+license.write("SIGNATURE:\n" + signature)
---
+++ Validate.py
@@ -0,0 +1,53 @@
+﻿from sys import exit
+import re
+from datetime import date, datetime
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA
+
+key = ""
+expDate = ""
+
+# Open license file
+try:
+    license = open('license').read()
+except:
+    print "License file not found"
+    exit(0)
+
+# Check expiration date, exit if expired or does not exist
+dateMatch = re.search(r'(?<=EXP_DATE:\n)\d{4}-\d{2}-\d{2}', license)
+if dateMatch:
+    expDate = dateMatch.group()
+    if datetime.strptime(expDate, '%Y-%m-%d').date() < date.today():
+        print "Invalid license: expired"
+        exit(0)
+else:
+    exit(0)
+
+# Find the public key, exit if it does not exist
+keyMatch = re.search(r'(?<=KEY:\n)-----BEGIN PUBLIC KEY-----.+-----END PUBLIC KEY-----', license, flags=re.DOTALL)
+if keyMatch:
+    key = RSA.importKey(keyMatch.group())
+else:
+    exit(0)
+
+sigMatch = re.search(r'(?<=SIGNATURE:\n).+', license, flags=re.DOTALL)
+if sigMatch:
+    signature = sigMatch.group()
+else:
+    exit(0)
+# Generate hash of the license
+hashLcn = SHA.new(expDate)
+
+# Create verification object using the public key
+verifier = PKCS1_v1_5.new(key)
+
+# Verify
+if verifier.verify(hashLcn, signature):
+    print "License accepted"
+    print "Hello World!"
+    exit(0)
+else:
+    print "Invalid license: integrity check failed"
+    exit(1)
